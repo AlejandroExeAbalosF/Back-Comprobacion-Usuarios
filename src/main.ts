29320531a681379b53cc78b9ec1505b3ffbbea7b
Alejandro Abalos Flores <alejandroexe96@gmail.com>
2025-01-23 13:33:37 -0300

    Comment

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+   // // Habilitar CORS solamente para recibir peticiones del front
+  // app.enableCors({
+  //   origin: ['http://localhost:3000', 'https://sih-back.onrender.com'],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  // });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
