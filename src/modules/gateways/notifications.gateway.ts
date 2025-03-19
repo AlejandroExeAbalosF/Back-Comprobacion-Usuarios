dad04e21add35073aacb6d8eb08fabc4f6caa11d
Alejandro E Abalos <alejandroexe96@gmail.com>
2025-03-19 12:09:38 -0300

    fix: prueba de correccion de webSocketServer

--- src/modules/gateways/notifications.gateway.ts
+++ src/modules/gateways/notifications.gateway.ts
@@ -1,8 +1,15 @@
 import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig({ path: '.env' });
 
 @WebSocketGateway({
-  cors: { origin: '*' }, // Ajusta el CORS según tu frontend
+  cors: {
+    origin: process.env.URL_FRONTEND, // Cambia esto a la URL de tu frontend en producción
+    credentials: true, // Permite cookies si las usas
+  },
+  transports: ['websocket'], // Asegura que se use WebSockets directamente
 })
 export class NotificationsGateway {
   @WebSocketServer()
@@ -10,6 +17,7 @@ export class NotificationsGateway {
 
   //
   sendNotification(data: unknown) {
+    console.log('Enviando notificación:', data);
     this.server.emit('employeeValidated', data); // Envía la notificación a todos los clientes
   }
 }
