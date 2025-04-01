7dc8154e233144b2e06e7b08268f61ad09ab8f8f
Alejandro E Abalos <alejandroexe96@gmail.com>
2025-04-01 15:11:38 -0300

    prueba deshabilitación de websocket

--- src/modules/gateways/notifications.gateway.ts
+++ src/modules/gateways/notifications.gateway.ts
@@ -4,20 +4,20 @@ import { config as dotenvConfig } from 'dotenv';
 
 dotenvConfig({ path: '.env' });
 
-@WebSocketGateway({
-  cors: {
-    origin: process.env.URL_FRONTEND, // Cambia esto a la URL de tu frontend en producción
-    credentials: true, // Permite cookies si las usas
-  },
-  transports: ['websocket', 'polling'], // Asegura que se use WebSockets directamente
-})
+// @WebSocketGateway({
+//   cors: {
+//     origin: process.env.URL_FRONTEND, // Cambia esto a la URL de tu frontend en producción
+//     credentials: true, // Permite cookies si las usas
+//   },
+//   transports: ['websocket', 'polling'], // Asegura que se use WebSockets directamente
+// })
 export class NotificationsGateway {
   @WebSocketServer()
   private server: Server;
 
   //
   sendNotification(data: unknown) {
-    // console.log('Enviando notificación:', data);
+    console.log('Enviando notificación:', data);
     this.server.emit('employeeValidated', data); // Envía la notificación a todos los clientes
   }
 }
